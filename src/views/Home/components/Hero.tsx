d2f26941a4f8e9b7a607848d833b1e23cede21e9
Diego Gomez <diego.gomez2005@gmail.com>
2021-09-26 19:35:00 -0700

    Improved ui

--- src/views/Home/components/Hero.tsx
+++ src/views/Home/components/Hero.tsx
@@ -107,10 +107,10 @@ const Hero = () => {
       >
         <Flex flex="1" flexDirection="column">
           <Heading scale="xxl" color="secondary" mb="24px">
-            {t('The moon is made of pancakes.')}
+            {t('Welcome to LunaSwap, powered by PancakeSwap')}
           </Heading>
           <Heading scale="md" mb="24px">
-            {t('Trade, earn, and win crypto on the most popular decentralized platform in the galaxy.')}
+            {t('The most powerful decentralized exchange out there, now in your hands')}
           </Heading>
           <Flex>
             {!account && <ConnectWalletButton mr="8px" />}
